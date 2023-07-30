fdba838dc26c370636f95f8fa44b3e17560c8ca9
Eric Fischer <efischer78@yahoo.fr>
2023-07-30 09:05:08 +0200

    ok

--- templates/timezone.sh
+++ templates/timezone.sh
@@ -6,4 +6,5 @@ tzdata tzdata/Areas select {{ .timezone.region }}
 tzdata tzdata/Zones/Europe select {{ .timezone.city }}
 EOF
 debconf-set-selections /tmp/tzdata.txt
-
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+rm /tmp/tzdata.txt
